b396414d3ecc072ffefad4d220c26565216c1ad1
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-05-06 22:11:01 +0100

    Removed unecessary if from logs

--- logging/lit.go
+++ logging/lit.go
@@ -9,29 +9,22 @@ const (
 )
 
 func init() {
+	lit.Prefix = ""
 	lit.LogLevel = lit.LogDebug
 }
 
 func Debug(format string, a ...interface{}) {
-	if lit.LogLevel == lit.LogDebug {
-		lit.Custom(lit.Writer, lit.LogDebug, callDepth, format, a...)
-	}
+	lit.Custom(lit.Writer, lit.LogDebug, callDepth, format, a...)
 }
 
 func Info(format string, a ...interface{}) {
-	if lit.LogLevel >= lit.LogInformational {
-		lit.Custom(lit.Writer, lit.LogInformational, callDepth, format, a...)
-	}
+	lit.Custom(lit.Writer, lit.LogInformational, callDepth, format, a...)
 }
 
 func Warn(format string, a ...interface{}) {
-	if lit.LogLevel >= lit.LogWarning {
-		lit.Custom(lit.Writer, lit.LogWarning, callDepth, format, a...)
-	}
+	lit.Custom(lit.Writer, lit.LogWarning, callDepth, format, a...)
 }
 
 func Error(format string, a ...interface{}) {
-	if lit.LogLevel >= lit.LogError {
-		lit.Custom(lit.Writer, lit.LogError, callDepth, format, a...)
-	}
+	lit.Custom(lit.Writer, lit.LogError, callDepth, format, a...)
 }
