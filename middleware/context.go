4fa2c2dd63abe344c77e60644b69619c7d99b81e
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-06-03 19:52:48 +0100

    Fixed context middleware func signature

--- middleware/context.go
+++ middleware/context.go
@@ -7,12 +7,10 @@ import (
 )
 
 // WithContext allows you to add a context timeout to a request
-func WithContext(next http.Handler, timeout time.Duration) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx, cancel := context.WithTimeout(r.Context(), timeout)
-			defer cancel()
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
+func WithContext(next http.HandlerFunc, timeout time.Duration) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), timeout)
+		defer cancel()
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
